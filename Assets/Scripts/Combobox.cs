dc1a7aff2d1c5bf01223affb27aff7a98a90ee2b
Dorian Roques <dorian-roques@orange.fr>
2023-04-23 15:08:55 +0200

    Clean code and simplify behaviour

--- Assets/Scripts/Combobox.cs
+++ Assets/Scripts/Combobox.cs
@@ -1,9 +1,6 @@
 using System.Linq;
-using System;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
 public class Combobox : TMP_Dropdown
@@ -11,10 +8,6 @@ public class Combobox : TMP_Dropdown
     [SerializeField]
     private TMP_InputField _searchField;
 
-    private List<OptionData> _persistentOptions;
-    private int _persistentValue;
-
-    private bool _isFocused;
     private bool _maintainSearchEditOnCurrentFrame;
 
     protected override void Awake()
@@ -23,19 +16,12 @@ public class Combobox : TMP_Dropdown
 
         _searchField = GetComponentInChildren<TMP_InputField>();
 
-        _persistentOptions = new List<OptionData>();
-        _persistentValue = 0;
-
-        _isFocused = false;
         _maintainSearchEditOnCurrentFrame = false;
     }
 
     protected override void OnEnable()
     {
         base.OnEnable();
-
-        _isFocused = false;
-        _maintainSearchEditOnCurrentFrame = false;
 
         _searchField.onValueChanged.AddListener(Handle_OnSearchChanged);
         _searchField.onEndEdit.AddListener(Handle_OnSearchEnded);
@@ -49,8 +35,23 @@ public class Combobox : TMP_Dropdown
         _searchField.onEndEdit.RemoveListener(Handle_OnSearchEnded);
     }
 
+    public new void Show()
+    {
+        base.Show();
+    }
+
+    public new void Hide()
+    {
+        base.Hide();
+    }
+
     public override void OnPointerClick(PointerEventData eventData)
     {
+        foreach (DropdownItem item in GetItems(true))
+        {
+            item.gameObject.SetActive(true);
+        }
+
         base.OnPointerClick(eventData);
         Focus();
     }
@@ -58,7 +59,7 @@ public class Combobox : TMP_Dropdown
     public override void OnSubmit(BaseEventData eventData)
     {
         base.OnSubmit(eventData);
-        Focus();
+        UnFocus();
     }
 
     public override void OnCancel(BaseEventData eventData)
@@ -69,14 +70,14 @@ public class Combobox : TMP_Dropdown
 
     private void Focus()
     {
-        if (!_isFocused)
+        Canvas listCanvas = GetComponentInChildren<Canvas>(false);
+
+        if (listCanvas == null)
         {
-            _persistentOptions = options.ToList();
-            _isFocused = true;
+            RefreshDropdownVisibility();
         }
 
         RefreshOptions();
-        ApplyPersistentValue();
 
         SelectSearchField();
         _maintainSearchEditOnCurrentFrame = true;
@@ -91,134 +92,97 @@ public class Combobox : TMP_Dropdown
         }
 
         Hide();
-
-        options = _persistentOptions.ToList();
-        _isFocused = false;
     }
 
     private void RefreshOptions()
     {
         string searchTerm = TextToTerm(_searchField.text);
+        DropdownItem[] items = GetItems(true);
 
         if (!string.IsNullOrEmpty(searchTerm))
         {
-            options = _persistentOptions
-                .Where(persistentOption =>
-                {
-                    string persistenTerm = TextToTerm(persistentOption.text);
-                    return persistenTerm.Contains(searchTerm);
-                })
-                .ToList();
-
-            RefreshDropdown();
-            ApplyPersistentValue();
-            
-            DropdownItem[] items = GetItems(false);
-
             for (int i = 0; i < items.Length; i++)
             {
                 DropdownItem item = items[i];
-                int itemIndex = i;
-                
-                item.toggle.onValueChanged.RemoveAllListeners();
-                item.toggle.onValueChanged.AddListener((isOn) => Handle_OnSelectDropdownItem(item.text.text));
+
+                bool isTemplateItem = (i == 0);
+                bool isReferenceItem = (i == 1);
+
+                if (isTemplateItem || isReferenceItem)
+                {
+                    item.gameObject.SetActive(true);
+                }
+                else
+                {
+                    string itemTerm = TextToTerm(item.text.text);
+                    item.gameObject.SetActive(itemTerm.Contains(searchTerm));
+                }
             }
+
+            RefreshDropdownList();
         }
         else
         {
-            options = _persistentOptions.ToList();
+            foreach (DropdownItem item in items)
+            {
+                item.gameObject.SetActive(true);
+            }
 
-            RefreshDropdown();
-            ApplyPersistentValue();
+            RefreshDropdownList();
         }
     }
 
-    private void RefreshDropdown()
+    private void RefreshDropdownVisibility()
     {
         float backupAphaFadeSpeed = alphaFadeSpeed;
+        {
+            alphaFadeSpeed = float.PositiveInfinity;
+            Hide();
 
-        alphaFadeSpeed = float.PositiveInfinity;
-
-        Hide();
-
-        alphaFadeSpeed = 0;
-
-        Show();
-
+            alphaFadeSpeed = 0;
+            Show();
+        }
         alphaFadeSpeed = backupAphaFadeSpeed;
+    }
+
+    private void RefreshDropdownList()
+    {
+        DropdownItem[] items = GetItems(false);
+        RectTransform templateItem = (RectTransform)items.First().transform;
+        RectTransform listTransform = (RectTransform)templateItem.parent;
+
+        float itemsHeight = templateItem.sizeDelta.y;
+
+        templateItem.anchoredPosition = new Vector2
+        {
+            x = templateItem.anchoredPosition.x,
+            y = listTransform.sizeDelta.y + itemsHeight * 0.5f
+        };
+
+        for (int i = 1; i < items.Length; i++)
+        {
+            DropdownItem item = items[i];
+            RectTransform itemTransform = (RectTransform)item.transform;
+
+            itemTransform.anchoredPosition = new Vector2
+            {
+                x = itemTransform.anchoredPosition.x,
+                y = listTransform.sizeDelta.y - itemsHeight * (i - 1) - itemsHeight * 0.5f
+            };
+        }
     }
 
     private void SelectSearchField()
     {
         int backupSelectionAnchorPosition = _searchField.selectionAnchorPosition;
         int backupSelectionStringAnchorPosition = _searchField.selectionStringAnchorPosition;
-
-        _searchField.Select();
-
+        {
+            _searchField.Select();
+        }
         _searchField.selectionAnchorPosition = backupSelectionAnchorPosition;
         _searchField.selectionStringAnchorPosition = backupSelectionStringAnchorPosition;
     }
 
-    private void StorePersistentValue(string selectedText)
-    {
-        string selectedTerm = TextToTerm(selectedText);
-
-        // Find index of selected value among persistent options
-        _persistentValue = _persistentOptions
-            .Select((persistentOption, index) => new
-            {
-                Option = persistentOption,
-                Index = index
-            })
-            .Where(optionToIndex =>
-            {
-                string optionTerm = TextToTerm(optionToIndex.Option.text);
-                return optionTerm.Equals(selectedTerm);
-            })
-            .Select(optionToIndex => optionToIndex.Index)
-            .DefaultIfEmpty(0)
-            .FirstOrDefault();
-    }
-
-    private void ApplyPersistentValue()
-    {
-        string persistentTerm = TextToTerm(_persistentOptions[_persistentValue].text);
-
-        // Find index of persistent value among filtered options
-        int newValue = options
-            .Select((option, index) => new
-            {
-                Option = option,
-                Index = index
-            })
-            .Where(optionToIndex =>
-            {
-                string optionTerm = TextToTerm(optionToIndex.Option.text);
-                return optionTerm.Equals(persistentTerm);
-            })
-            .Select(optionToIndex => optionToIndex.Index)
-            .DefaultIfEmpty(-1)
-            .FirstOrDefault();
-
-        SetValueWithoutNotify(newValue);
-
-        Toggle[] toggles = GetToggles(false);
-
-        for (int i = 0; i < toggles.Length; i++)
-        {
-            Toggle toggle = toggles[i];
-            TMP_Text toggleText = toggle.GetComponentInChildren<TMP_Text>();
-
-            string toggleTerm = TextToTerm(toggleText.text);
-            toggle.SetIsOnWithoutNotify(toggleTerm.Equals(persistentTerm));
-        }
-    }
-
-    private Toggle[] GetToggles(bool includeInactive)
-    {
-        return GetComponentsInChildren<Toggle>(includeInactive);
-    }
-    
     private DropdownItem[] GetItems(bool includeInactive)
     {
         return GetComponentsInChildren<DropdownItem>(includeInactive);
@@ -227,14 +191,6 @@ public class Combobox : TMP_Dropdown
     private string TextToTerm(string text)
     {
         return text.ToLower().Trim();
-    }
-
-    private void Handle_OnSelectDropdownItem(string selectedText)
-    {
-        StorePersistentValue(selectedText);
-        RefreshDropdown();
-        ApplyPersistentValue();
-        Hide();
     }
 
     private void Handle_OnSearchChanged(string newValue)
